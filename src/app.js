7e300e16aef08e69d57b8c727e5cdbe2ce1fda6a
Bruno <b.deszczynski@gmail.com>
2026-06-16 11:28:09 +0400

    Revert "Add admin result builder"
    This reverts commit 9dd88b71826797500a4ea81552d6590f24f1a565.

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import {
 import { flagUrlForTeam } from "./flags.js?v=34";
 
 const DATA_URL = new URL("../public/data/world-cup.json", import.meta.url);
-const APP_VERSION = "v66-admin-result-builder";
+const APP_VERSION = "v65-admin-page";
 
 const state = {
   data: null,
